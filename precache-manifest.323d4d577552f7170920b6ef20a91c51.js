ece92972b199a162da860aeb7fbb049be1800b09
Kike Méndez <luis17erebsamen@gmail.com>
2022-02-23 16:56:55 -0600

    Updates

---
+++ precache-manifest.323d4d577552f7170920b6ef20a91c51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f556b2bb47415d050513e231170d80",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "f2bc6b080cd9ccda21ed",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c2688147ed37a6bd8c87",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/static/css/main.c13d16e9.chunk.css"
+  },
+  {
+    "revision": "f2bc6b080cd9ccda21ed",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/static/js/2.09ca5df0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/static/js/2.09ca5df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2688147ed37a6bd8c87",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/static/js/main.1319d679.chunk.js"
+  },
+  {
+    "revision": "48433d25d2b877dad9dd",
+    "url": "/Kikeme98/react-frontend-dev-portfolio/static/js/runtime-main.2caf5fe6.js"
+  }
+]);
